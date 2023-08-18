b6240d91390fece0c4ec89d6b9e11a7de7c4e0ec
importOnurDogan <122278023+importOnurDogan@users.noreply.github.com>
2023-08-18 15:58:10 +0300

    Update7

--- 2SUM.py
+++ 2SUM.py
@@ -14,9 +14,7 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in nums:
             for j in nums:
-                if i + j == target:
-                    len(nums)
-                    numbers = [nums.index(i), nums.index(j)]
-                    return numbers
+                if nums.index(i) != nums.index(j) and i + j == target:
+                    return [nums.index(i), nums.index(j)]
                 else:
                     continue
