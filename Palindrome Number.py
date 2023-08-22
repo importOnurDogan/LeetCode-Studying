127f0b8ec51e8607c0729d2edd5d75b7e6b5c41c
importOnurDogan <122278023+importOnurDogan@users.noreply.github.com>
2023-08-23 00:45:11 +0300

    Update2
    still not working

--- Palindrome Number.py
+++ Palindrome Number.py
@@ -1,4 +1,4 @@
-"Given an integer X, return TRUE if X is a -palindrome- and FALSE otherwise."
+"Given an integer x, return true if x is a -palindrome- and false otherwise."
 """ 
 1: Example
 Input: x = 121
@@ -8,13 +8,20 @@ Explanation: 121 reads as 121 from left to right and from right to left.
 
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        c = []
+
+        if x < 0 or x == 10:
+            return False
+        if 0 <= x < 10:
+            return True
         a = 10
-        while True:
-            if x > a:
-                c.append(x % a)
-                a = a * 10
-            else:
-                break
-        c = bool(c)
-        return c
+        new_x = []
+        while (x / a) > a:
+            new_x.append(x % a)
+            new_x.append((x / a) % a)
+            a = a * 10
+            new_x.append(x / a)
+        new_x = bool(new_x)
+        if x == new_x:
+            return True
+
+
