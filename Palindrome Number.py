b5cca403632f89f5c3f7e07774a04bdd00e7665e
importOnurDogan <122278023+importOnurDogan@users.noreply.github.com>
2023-08-23 10:41:24 +0300

    Update3
    Final

--- Palindrome Number.py
+++ Palindrome Number.py
@@ -9,19 +9,12 @@ Explanation: 121 reads as 121 from left to right and from right to left.
 class Solution:
     def isPalindrome(self, x: int) -> bool:
 
-        if x < 0 or x == 10:
-            return False
-        if 0 <= x < 10:
-            return True
-        a = 10
-        new_x = []
-        while (x / a) > a:
-            new_x.append(x % a)
-            new_x.append((x / a) % a)
-            a = a * 10
-            new_x.append(x / a)
-        new_x = bool(new_x)
-        if x == new_x:
-            return True
+        newx = x
+        back_x = 0
+        while x > 0:
+            back_x = (back_x * 10) + (x % 10)
+            x = x // 10
+        return newx == back_x
+
 
 
